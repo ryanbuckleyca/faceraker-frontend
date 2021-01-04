19a76ef7646b02d5ebd056d0a7fee59dbe64d240
Ryan Buckley <ryanbuckley@gmail.com>
2021-01-04 09:55:06 -0500

    determine url based on node_env

--- src/utils/callAPI.js
+++ src/utils/callAPI.js
@@ -1,12 +1,16 @@
 import { errors } from './errorHandler'
 
+const baseURL = process.env.NODE_ENV === 'development'
+? "http://localhost:8000/"
+: "http://louwer.herokuapp.com/"
+
 export const callAPI = async (q) => {
   const options = {
     method: 'POST',
     headers: { 'content-type': 'application/json'}
   }
   try {
-    const res = await fetch(`http://localhost:8000/graphql?${q}`, options)
+    const res = await fetch(`${baseURL}graphql?${q}`, options)
     const json = await res.json()
     console.log(json)
     return json
